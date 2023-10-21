7caf5d84742f5ee2bada92557f0002c91268866b
Varad Patil <varadapatil123@gmail.com>
2023-10-21 22:33:26 +0530

    minor fixes

--- components/CreatePlaylistButton.tsx
+++ components/CreatePlaylistButton.tsx
@@ -12,7 +12,7 @@ const CreatePlaylistButton = () => {
   return (
     <button
       onClick={onClick}
-      className="bg-blue-500 hidden md:flex text-lg hover:bg-blue-700 transition text-white justify-center items-center gap-2 font-bold py-2 px-4 rounded-[20px]"
+      className="bg-blue-500 hidden md:flex text-lg hover:bg-blue-700 transition text-white justify-center items-center gap-2 font-bold py-2 px-4 rounded-full"
     >
       <AiOutlinePlus size={24} /> Playlist
     </button>
